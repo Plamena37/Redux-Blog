04e7f8695347cf10b013176ff44144025ae9f645
Plamena <yomena37@gmail.com>
2023-02-07 15:06:16 +0200

    implement styling

--- src/features/users/UserDetail.js
+++ src/features/users/UserDetail.js
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux";
 import { selectUserById } from "../users/usersSlice";
 import { selectPostsByUser } from "../posts/postsSlice";
 import { Link, useParams } from "react-router-dom";
+import Person4Icon from "@mui/icons-material/Person4";
+import "./UserDetail.scss";
 
 const UserDetail = () => {
   const { userId } = useParams();
@@ -12,16 +14,18 @@ const UserDetail = () => {
   );
 
   const postTitles = postsForUser.map((post) => (
-    <li key={post.id}>
+    <li key={post.id} className="user__post__link">
       <Link to={`/post/${post.id}`}>{post.title}</Link>
     </li>
   ));
 
   return (
-    <section>
-      <h2>{user?.name}</h2>
-
-      <ol>{postTitles}</ol>
+    <section className="layout__container section__user">
+      <div className="section__user__heading">
+        <h2>{user?.name}</h2>
+        <Person4Icon className="section__user__heading--icon" />
+      </div>
+      <ol className="user__post">{postTitles}</ol>
     </section>
   );
 };

149397745eb59296ee351374b0a50f438e54a21a
Plamena <yomena37@gmail.com>
2023-02-12 15:26:53 +0200

    code cleanup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import UserDetail from "./features/users/UserDetail";
 import Layout from "./components/Layout";
 import PostDetail from "./features/posts/PostDetail";
 import Error from "./components/Error";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 
 const App = () => {
